da9d1b52425a7be0901a8495d75f73e7cccc854f
renil <renilbabu03@gmail.com>
2017-01-15 12:59:00 +0530

    bug solved

--- src/scope.js
+++ src/scope.js
@@ -38,6 +38,7 @@ Scope.prototype.$digest = function() {
     var ttl = 10;
     var dirty;
     this.$$lastDirtyWatch = null;
+    this.$beginPhase("$digest");
     do {
         while (this.$$asyncQueue.length) {
             var asyncTask = this.$$asyncQueue.shift();
@@ -45,9 +46,11 @@ Scope.prototype.$digest = function() {
         }
         dirty = this.$$digestOnce();
         if ((dirty || this.$$asyncQueue.length) && !(ttl--)) {
-            throw "10 digest iterations reached"
+            this.$clearPhase();
+            throw "10 digest iterations reached";
         }
     } while (dirty || this.$$asyncQueue.length);
+    this.$clearPhase();
 };
 Scope.prototype.$$areEqual = function(newValue, oldValue, valueEq) {
     if (valueEq) {
@@ -61,15 +64,17 @@ Scope.prototype.$$areEqual = function(newValue, oldValue, valueEq) {
 
 Scope.prototype.$eval = function(expr, locals) {
     return expr(this, locals);
-}
+};
 
 Scope.prototype.$apply = function(expr) {
     try {
+        this.$beginPhase("$apply");
         return this.$eval(expr);
     } finally {
+        this.$clearPhase();
         this.$digest();
     }
-}
+};
 
 Scope.prototype.$evalAsync = function(expr) {
     var self = this;
@@ -78,10 +83,21 @@ Scope.prototype.$evalAsync = function(expr) {
             if (self.$$asyncQueue.length) {
                 self.$digest();
             }
-        }, 0)
+        }, 0);
     }
     self.$$asyncQueue.push({
         scope: self,
         expression: expr
-    })
-}
+    });
+};
+
+Scope.prototype.$beginPhase = function(phase) {
+    if (this.$$phase) {
+        throw this.$$phase + ' already in progress';
+    }
+    this.$$phase = phase;
+};
+
+Scope.prototype.$clearPhase = function() {
+    this.$$phase = null;
+};
